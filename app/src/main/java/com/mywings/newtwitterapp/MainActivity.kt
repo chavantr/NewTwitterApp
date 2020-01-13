7f82e94cc04293dca6fc08f9cfd00da335c58ca9
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2020-01-14 04:13:28 +0530

    filter activity

--- app/src/main/java/com/mywings/newtwitterapp/MainActivity.kt
+++ app/src/main/java/com/mywings/newtwitterapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.mywings.newtwitterapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSearch.setOnClickListener {
+            if (!txtSearch.text.toString().isNullOrEmpty()) {
+                val intent = Intent(this@MainActivity, FilterActivity::class.java)
+                intent.putExtra("keyword", txtSearch.text.toString())
+                startActivity(intent)
+            } else {
+                Toast.makeText(this@MainActivity, "Keyword should not empty", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
